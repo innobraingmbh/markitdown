8baa451b80f814adf8835bc0ef5c6fb29761ccc2
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-12-16 17:13:48 +0100

    fix: make executable configurable

--- config/markitdown.php
+++ config/markitdown.php
@@ -4,5 +4,14 @@ declare(strict_types=1);
 
 // config for Innobrain/Markitdown
 return [
-    'process_timeout' => 30,
+    /*
+     * Use this to set the timeout for the process. Default is 30 seconds.
+     */
+    'process_timeout' => env('MARKITDOWN_PROCESS_TIMEOUT', 30),
+
+    /*
+     * Use this to set the path to the markitdown executable. If not set,
+     * the binary will be searched in the PATH.
+     */
+    'executable' => env('MARKITDOWN_EXECUTABLE', 'markitdown'),
 ];
